d2933e4c19edae7cc02c516435ef1fed8038373a
duilec <elelsyqrlc42@163.com>
2022-07-05 10:49:50 +0800

    update comments in bio.c

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,16 +18,24 @@ struct run {
   struct run *next;
 };
 
-struct {
+// we must have the type_name(Keme) so we can establish the array of struct(kmems[NCPU])
+struct Keme{
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+};
+
+struct Keme kmems[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  // set eight "kmem" for eight cpus
+  for(int i = 0; i < NCPU; i++){
+    initlock(&kmems[i].lock, "kmem");
+      // we build a single freelist to kmems[0] firstly 
+      if (i == 0) 
+        freerange(end, (void*)PHYSTOP);
+  }
 }
 
 void
@@ -56,10 +64,16 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  // get current cpu_id
+  push_off();
+  int cpu_id = cpuid(); 
+  pop_off();
+
+  // free a page from freelist of cpu_id 
+  acquire(&kmems[cpu_id].lock);
+  r->next = kmems[cpu_id].freelist;
+  kmems[cpu_id].freelist = r;
+  release(&kmems[cpu_id].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,13 +84,52 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  // get current cpu_id
+  push_off();
+  int cpu_id = cpuid();
+  pop_off();
 
-  if(r)
+  acquire(&kmems[cpu_id].lock); // use 'acquire' also disable intrruption
+
+  r = kmems[cpu_id].freelist;
+  
+  // if freelist of current cpu_id has freepage we use it
+  if(r){
+    kmems[cpu_id].freelist = r->next;
+    release(&kmems[cpu_id].lock);
     memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+    return (void*)r;
+  }
+
+  // else we steal freepage from other freelist of cpus
+  else{
+    release(&kmems[cpu_id].lock);
+    for(int i = 0; i < NCPU; i++){
+      // avoid race condition(same cpu_id lock)
+      if (i != cpu_id){ 
+        acquire(&kmems[i].lock);
+
+        // the last_list also not memory so ret 0 
+        if(i == NCPU - 1 && kmems[i].freelist == 0){  
+          release(&kmems[i].lock);
+          return (void*)0;
+        }
+
+        // It is OK to allocte a freepage by stealing from other cpus
+        if(kmems[i].freelist){
+          struct run *to_alloc = kmems[i].freelist; 
+          kmems[i].freelist = to_alloc->next;
+          release(&kmems[i].lock);
+          memset((char*)to_alloc, 5, PGSIZE); // fill with junk
+          return (void*)to_alloc;
+        }
+
+        // if no matching condition, we must release current lock
+        release(&kmems[i].lock); 
+      }
+    }
+  }
+
+  // Returns 0 if the memory cannot be allocated. 
+  return (void*)0;
 }
