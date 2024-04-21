d2b9e341e605aa5abf77ad629aedc6dd26ce18bf
duilec <elelsyqrlc42@163.com>
2024-04-21 15:59:28 +0800

    modiyied layout in sleep.c

--- user/sleep.c
+++ user/sleep.c
@@ -2,15 +2,15 @@
 #include"kernel/stat.h"
 #include"user/user.h"
 
-int main(int agrc, char* agrv[]){
-	/* argc is the number of command, argv[] store the context of parameters */
-	if(agrc == 1){
-		printf("Please enter parameter of int");
-	}
-	else 
-    {
-		int time = atoi(agrv[1]);
-		sleep(time);
-	}
-	exit(0);
+int
+main(int agrc, char* agrv[]){
+  /* argc is the number of commands*/
+  /* argv[] store the context of parameters */
+  if(agrc == 1){
+    printf("Please enter parameter of int");
+  }else {
+    int time = atoi(agrv[1]);
+    sleep(time);
+  }
+    exit(0);
 }
